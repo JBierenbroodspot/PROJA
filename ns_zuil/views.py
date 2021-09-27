488480d87c5ae73422601f0c6cd0c47ad5ce8ba5
JBierenrboodspot <jim12.brood@gmail.com>
2021-09-27 12:14:11 +0200

    Add templates for MessageView and ModeratorView

--- ns_zuil/views.py
+++ ns_zuil/views.py
@@ -1,3 +1,19 @@
+import django.views
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+class MessageView(django.views.View):
+    TEMPLATE = r""
+
+    def get(self, request: object, *args, **kwargs) -> HttpResponse:
+        context: dict[object] = {}
+        return render(request, self.TEMPLATE, context)
+
+
+class ModeratorView(django.views.View):
+    TEMPLATE = r""
+
+    def get(self, request: object, *args, **kwargs) -> HttpResponse:
+        context: dict[object] = {}
+        return render(request, self.TEMPLATE, context)
