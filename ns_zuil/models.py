0139ac9a65571ea57d8857b28a848ed36d2d58ab
JBierenrboodspot <jim12.brood@gmail.com>
2021-09-29 14:46:51 +0200

    Add Message and Station models to models.py

--- ns_zuil/models.py
+++ ns_zuil/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    message: models.CharField = models.CharField(max_length=140)
+    firstname: models.CharField = models.CharField(max_length=255, default="A.")
+    insertion: models.CharField = models.CharField(max_length=255, blank=True, null=True)
+    lastname: models.CharField = models.CharField(max_length=255, default="Noniem")
+    post_datetime: models.DateTimeField = models.DateTimeField(auto_now=True)
+    moderated: models.BooleanField = models.BooleanField(default=False)
+    moderation_datetime: models.DateTimeField = models.DateTimeField(blank=True, null=True)
+    station_fk: models.ForeignKey = models.ForeignKey("Station", on_delete=models.CASCADE)
+    moderated_by_fk: models.ForeignKey = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return f"Message {self.id}"
+
+
+class Station(models.Model):
+    name: models.CharField = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"Station {self.name}"
