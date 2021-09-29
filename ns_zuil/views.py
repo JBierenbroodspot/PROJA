355dfd32305d70b6231c7cd22b9b1d0d028a2bb0
JBierenrboodspot <jim12.brood@gmail.com>
2021-09-29 14:48:54 +0200

    Add MessageView and ChooseStationView to views.py

--- ns_zuil/views.py
+++ ns_zuil/views.py
@@ -1,23 +1,43 @@
+from typing import overload, Any
+
 import django.views
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+from ns_zuil import models, forms
 
-class MessageView(django.views.View):
-    TEMPLATE = r"templates/message_view.html"
 
-    def get(self, request: object, station: str) -> HttpResponse:
-        context: dict[object] = {}
-        if station:
-            pass
-        else:
-            pass
-        return render(request, self.TEMPLATE, context)
+class MessageView(django.views.generic.edit.FormView):
+    template_name = "write_message.html"
+    form_class = forms.MessageForm
+    success_url = "#"
+
+    def form_valid(self, form):
+        cleaned: dict[Any] = form.cleaned_data
+        message = models.Message(message=cleaned["message"],
+                                 firstname=cleaned["firstname"],
+                                 insertion=cleaned["insertion"],
+                                 lastname=cleaned["lastname"],
+                                 station_fk_id=int(self.request.session.get("station_id")))
+        message.save()
+        return super().form_valid(form)
+
+
+class ChooseStationView(django.views.generic.edit.FormView):
+    template_name = "select_station.html"
+    form_class = forms.StationForm
+    success_url = "station/"
+
+    def form_valid(self, form):
+        cleaned: dict[Any] = form.cleaned_data
+        self.request.session["station_id"] = cleaned["station"].pk
+        self.success_url += str(cleaned["station"].pk)
+        return super().form_valid(form)
 
 
 class ModeratorView(django.views.View):
     TEMPLATE = r""
 
-    def get(self, request: object, *args, **kwargs) -> HttpResponse:
-        context: dict[object] = {}
+    def get(self, request: Any, *args, **kwargs) -> HttpResponse:
+        context: dict[Any] = {}
         return render(request, self.TEMPLATE, context)
