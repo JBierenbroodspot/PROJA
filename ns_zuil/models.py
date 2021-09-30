f80f55c028b2cb1ff16b9ded468c435ad2e66d3d
JBierenrboodspot <jim12.brood@gmail.com>
2021-09-30 14:15:44 +0200

    Add verbose names to Message model fields

--- ns_zuil/models.py
+++ ns_zuil/models.py
@@ -7,11 +7,15 @@ class Message(models.Model):
     firstname: models.CharField = models.CharField(max_length=255, default="A.")
     insertion: models.CharField = models.CharField(max_length=255, blank=True, null=True)
     lastname: models.CharField = models.CharField(max_length=255, default="Noniem")
-    post_datetime: models.DateTimeField = models.DateTimeField(auto_now=True)
-    status: models.CharField = models.CharField(max_length=20, default="Pending")
-    moderation_datetime: models.DateTimeField = models.DateTimeField(blank=True, null=True)
-    station_fk: models.ForeignKey = models.ForeignKey("Station", on_delete=models.CASCADE)
-    moderated_by_fk: models.ForeignKey = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    post_datetime: models.DateTimeField = models.DateTimeField(auto_now=True, verbose_name="Posted on")
+    status: models.CharField = models.CharField(max_length=20, default="PENDING")
+    moderation_datetime: models.DateTimeField = models.DateTimeField(blank=True, null=True, verbose_name="Moderated on")
+    station_fk: models.ForeignKey = models.ForeignKey("Station", on_delete=models.CASCADE, verbose_name="Station")
+    moderated_by_fk: models.ForeignKey = models.ForeignKey(User,
+                                                           on_delete=models.SET_NULL,
+                                                           blank=True,
+                                                           null=True,
+                                                           verbose_name="Moderated by")
 
     def __str__(self):
         return f"Message {self.id}"
