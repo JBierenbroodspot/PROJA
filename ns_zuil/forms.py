bb50415fa2fc8f6a9915233335903a4a6ba48f91
JBierenrboodspot <jim12.brood@gmail.com>
2021-10-17 14:02:33 +0200

    Add comment field to form

--- ns_zuil/forms.py
+++ ns_zuil/forms.py
@@ -8,14 +8,15 @@ class StationForm(forms.Form):
 
 
 class MessageForm(forms.Form):
-    message = forms.CharField(max_length=140, widget=forms.Textarea(attrs={"rows": 4}))
-    firstname = forms.CharField(max_length=30, required=False)
-    insertion = forms.CharField(max_length=15, required=False)
-    lastname = forms.CharField(max_length=30, required=False)
+    message: forms.CharField = forms.CharField(max_length=140, widget=forms.Textarea(attrs={"rows": 4}))
+    firstname: forms.CharField = forms.CharField(max_length=30, required=False)
+    insertion: forms.CharField = forms.CharField(max_length=15, required=False)
+    lastname: forms.CharField = forms.CharField(max_length=30, required=False)
 
 
 class ModerationForm(forms.Form):
-    status = forms.ChoiceField(choices=(
+    status: forms.ChoiceField = forms.ChoiceField(choices=(
         ("ACCEPTED", "Accepteren"),
         ("DENIED", "Weigeren"),
     ))
+    comment: forms.CharField = forms.CharField(max_length=255, required=False)
